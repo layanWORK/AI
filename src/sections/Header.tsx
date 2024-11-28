9052e47d53f958658db569a6ba0d97c89659755d
layan alahmadi <layan.work099@gmail.com>
2024-11-28 21:54:54 +0300

    header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,27 +3,27 @@ import MenuIcon from "@/assets/icon-menu.svg";
 export const Header = () => {
   return (
     <>
-      <header className="py-4 border-b border-white/15">
+      <header className="py-4 border-b border-white/15 md:border-none">
         <div className=" container">
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
             <div>
               <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
                 <LogoIcon className="size-8" />{" "}
               </div>
             </div>
-<div>
-  <nav>
-    <a href="#">
+<div className="hidden md:block">
+  <nav className="flex gap-8 text-sm ">
+    <a href="#" className=" text-white/70 hover:text-white transition">
 Feachers
     </a>
-    <a href="#">
+    <a href="#" className=" text-white/70 hover:text-white transition">
 Devolpers
 </a>
-<a href="#">
+<a href="#" className=" text-white/70 hover:text-white transition">
 Priicing
 </a>
 
-<a href="#">
+<a href="#" className=" text-white/70 hover:text-white transition">
 Changple 
 </a>
   </nav>
@@ -38,7 +38,7 @@ Changple
                 </div>
                 <span>Join waitis</span>
               </button>
-              <MenuIcon />
+              <MenuIcon className=" md:hidden" />
             </div>
           </div>
         </div>
