3666fd0bd3e043c2769325e3fb775a60b03e8723
layan alahmadi <layan.work099@gmail.com>
2024-11-28 21:42:17 +0300

    fix

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -5,13 +5,29 @@ export const Header = () => {
     <>
       <header className="py-4 border-b border-white/15">
         <div className=" container">
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <div>
               <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
                 <LogoIcon className="size-8" />{" "}
               </div>
             </div>
+<div>
+  <nav>
+    <a href="#">
+Feachers
+    </a>
+    <a href="#">
+Devolpers
+</a>
+<a href="#">
+Priicing
+</a>
 
+<a href="#">
+Changple 
+</a>
+  </nav>
+</div>
             <div className="flex gap-4 items-center">
               <button className=" relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
                 <div className=" absolute inset-0  ">
