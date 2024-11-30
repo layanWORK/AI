6acbdce8f4080902877015a5862352bb722b5caa
layan alahmadi <layan.work099@gmail.com>
2024-11-30 20:33:48 +0300

    hero done

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,43 +1,55 @@
 import LogoIcon from "@/assets/logo.svg";
 import MenuIcon from "@/assets/icon-menu.svg";
+import  Button  from "@/components/button";
 export const Header = () => {
   return (
     <>
-      <header className="py-4 border-b border-white/15 md:border-none">
+      <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10 ">
+      <div className=" absolute inset-0 backdrop-blur md:hidden -z-10">
+
+      </div>
         <div className=" container">
-          <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
+          <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto  relative  ">
+          <div className=" absolute inset-0 backdrop-blur -z-10 hidden md:block ">
+
+          </div>
             <div>
               <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
                 <LogoIcon className="size-8" />{" "}
               </div>
             </div>
-<div className="hidden md:block">
-  <nav className="flex gap-8 text-sm ">
-    <a href="#" className=" text-white/70 hover:text-white transition">
-Feachers
-    </a>
-    <a href="#" className=" text-white/70 hover:text-white transition">
-Devolpers
-</a>
-<a href="#" className=" text-white/70 hover:text-white transition">
-Priicing
-</a>
+            <div className="hidden md:block">
+              <nav className="flex gap-8 text-sm ">
+                <a
+                  href="#"
+                  className=" text-white/70 hover:text-white transition"
+                >
+                  Feachers
+                </a>
+                <a
+                  href="#"
+                  className=" text-white/70 hover:text-white transition"
+                >
+                  Devolpers
+                </a>
+                <a
+                  href="#"
+                  className=" text-white/70 hover:text-white transition"
+                >
+                  Priicing
+                </a>
 
-<a href="#" className=" text-white/70 hover:text-white transition">
-Changple 
-</a>
-  </nav>
-</div>
+                <a
+                  href="#"
+                  className=" text-white/70 hover:text-white transition"
+                >
+                  Changple
+                </a>
+              </nav>
+            </div>
             <div className="flex gap-4 items-center">
-              <button className=" relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
-                <div className=" absolute inset-0  ">
-                  <div className=" rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_botom,black, transparent)]">
-                    <div className=" rounded-lg border absolute inset-0 border-white/20 [mask-image:linear-gradient(to_top,black, transparent)]"></div>
-                    <div className=" absolute inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg"></div>
-                  </div>
-                </div>
-                <span>Join waitis</span>
-              </button>
+          <Button> Join withlist</Button>
+          
               <MenuIcon className=" md:hidden" />
             </div>
           </div>
