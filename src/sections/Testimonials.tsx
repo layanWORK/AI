cbe86edf103801b41ff1240294e84686427063e0
layan alahmadi <layan.work099@gmail.com>
2024-12-11 20:36:47 +0300

    Done aLL

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,13 +1,15 @@
+"use client"
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
 import Image from "next/image";
+import { motion  } from "motion/react";
 
 const testimonials = [
   {
     text: "“This product has completely transformed how I manage my projects and deadlines”",
-    name: "Sophia Perez",
+    name: "Sophia Perezq",
     title: "Director @ Quantum",
     avatarImg: avatar1,
   },
@@ -41,36 +43,48 @@ export const Testimonials = () => {
         <p className=" text-white/70 text-lg md:text-xl  text-center mt-5 tracking-tight max-w-sm mx-auto ">
           Our revolution AI SEO tools have transformed our clients strategies
         </p>
-<div className=" overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+        <div className=" flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+        <motion.div
 
+          className=" flex gap-5 pr-5 flex-none -translate-x-1/2"
+          initial={{ translateX: '-50%'}}
+          animate={{
+           translateX: '0',
+         }} 
+         transition={{
+           repeat: Infinity,
+           duration: 30,
+           ease: "linear"
+         }}
+        
+      >
+            {[...testimonials , ...testimonials].map((testimonial, index) => (
+              <div
+                key={`${testimonial.name}+ ${testimonial.title} + ${index}`}
+                className=" border border-white/15  p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgba(140,69,255,0.3),black)] max-w-xs md:max-w-md flex-none"
+              >
+                <div className=" text-lg md:text-2xl tracking-tight">
+                  {testimonial.text}
+                </div>
+                <div className=" flex items-center gap-3 mt-5">
+                  <div className=" relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:absolute before:content-[''] before:inset-0 before:border before:border-white/50 before:z-10 before:rounded-lg">
+                    <Image
+                      className=" size-11 rounded-lg grayscale  "
+                      src={testimonial.avatarImg}
+                      alt={`Afater for + ${testimonial.name}`}
+                    />
+                  </div>
 
-        <div className=" flex gap-5">
-
-      
-        {testimonials.map((testimonial) => (
-          <div
-            key={testimonial.name}
-            className=" border border-white/15  p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgba(140,69,255,0.3),black)] max-w-xs md:max-w-md flex-none"
-          >
-            <div className=" text-lg md:text-2xl tracking-tight">{testimonial.text}</div>
-            <div className=" flex items-center gap-3 mt-5">
-              <div className=" relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:absolute before:content-[''] before:inset-0 before:border before:border-white/50 before:z-10 before:rounded-lg">
-<Image
-              className=" size-11 rounded-lg grayscale  "
-                src={testimonial.avatarImg}
-                alt={`Afater for + ${testimonial.name}`}
-              />
-
+                  <div className=" flex flex-col ">
+                    <div>{testimonial.name}</div>
+                    <div className=" text-white/50 text-sm ">
+                      {testimonial.title}
+                    </div>
+                  </div>
+                </div>
               </div>
-
-              
-              <div className=" flex flex-col ">
-                <div>{testimonial.name}</div>
-                <div className=" text-white/50 text-sm ">{testimonial.title}</div>
-              </div>
-            </div>
-          </div>
-        ))}  </div>
+            ))}{" "}
+          </motion.div>
         </div>
       </div>
     </section>
